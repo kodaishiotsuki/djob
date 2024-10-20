f64b1a3c3e9578a64fa8b00f9d919883eba60515
Kodai <0711dr5@gmail.com>
2024-10-20 22:30:12 +0900

    fix:create static browseDetailpage

---
+++ djob_frontend/pages/browse/[id].vue
@@ -0,0 +1,33 @@
+<script setup></script>
+
+<template>
+  <div class="py-10 px-6 grid md:grid-cols-4 gap-3">
+    <div class="md:col-span-3">
+      <h1 class="mb-6 text-3xl">job.title</h1>
+
+      <p>job.description</p>
+
+      <a class="inline-block mt-6 py-4 px-6 bg-teal-700 text-white rounded-xl"
+        >Apply</a
+      >
+    </div>
+
+    <div class="md:col-span-1 p-6 bg-teal-700 text-white rounded-xl">
+      <h3 class="mb-6 text-2xl">Company</h3>
+
+      <p class="mb-2">job.company_name</p>
+      <p>Loca job.company_locatoin tion</p>
+
+      <hr class="my-6 opacity-30" />
+
+      <h3 class="mb-6 text-2xl">Position</h3>
+
+      <p class="mb-2">job.position_location</p>
+      <p>job.position_salary</p>
+
+      <hr class="my-6 opacity-30" />
+
+      <p>Posted at job.created_at_formatted</p>
+    </div>
+  </div>
+</template>
