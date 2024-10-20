2aba3acb9e853aa5bd6ab58854cd5c42a92f2d5f
Kodai <0711dr5@gmail.com>
2024-10-20 22:54:38 +0900

    fix:create job component

---
+++ djob_frontend/components/job.vue
@@ -0,0 +1,42 @@
+<script setup>
+const props = defineProps({
+  my: {
+    type: [Boolean],
+  },
+});
+</script>
+
+<template>
+  <div class="p-6 flex items-center justify-between bg-gray-100 rounded-xl">
+    <div>
+      <h3 class="mb-2 text-xl font-semibold">title</h3>
+      <p class="text-gray-600">job.company_name</p>
+    </div>
+
+    <div>
+      <p class="mb-2">job.position_location</p>
+      <p>job.position_salary</p>
+    </div>
+
+    <div>
+      <p>Posted job.created_at_formatted</p>
+    </div>
+
+    <div class="space-x-4">
+      <NuxtLink
+        v-bind:to="'/browse/' + 1"
+        class="py-4 px-6 bg-teal-700 text-white rounded-xl"
+        >Details</NuxtLink
+      >
+      <NuxtLink
+        v-bind:to="'/editjob/' + 1"
+        class="py-4 px-6 bg-cyan-700 text-white rounded-xl"
+        v-if="my"
+        >Edit</NuxtLink
+      >
+      <a class="py-4 px-6 bg-rose-700 text-white rounded-xl" v-if="my"
+        >Delete</a
+      >
+    </div>
+  </div>
+</template>
--- djob_frontend/pages/browse/index.vue
+++ djob_frontend/pages/browse/index.vue
@@ -39,33 +39,7 @@
 
     <div class="md:col-span-3">
       <div class="space-y-4">
-        <div class="space-y-4">
-          <div
-            class="p-6 flex items-center justify-between bg-gray-100 rounded-xl"
-          >
-            <div>
-              <h3 class="mb-2 text-xl font-semibold">title</h3>
-              <p class="text-gray-600">job.company_name</p>
-            </div>
-
-            <div>
-              <p class="mb-2">job.position_location</p>
-              <p>job.position_salary</p>
-            </div>
-
-            <div>
-              <p>Posted job.created_at_formatted</p>
-            </div>
-
-            <div class="space-x-4">
-              <NuxtLink
-                v-bind:to="'/browse/' + 1"
-                class="py-4 px-6 bg-teal-700 text-white rounded-xl"
-                >Details</NuxtLink
-              >
-            </div>
-          </div>
-        </div>
+        <Job />
       </div>
     </div>
   </div>
