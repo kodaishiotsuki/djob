fcf3e7e98096f51c7b5ca985e6f77da00e0a47de
Kodai <0711dr5@gmail.com>
2024-10-21 17:15:57 +0900

    fix:リフレッシュ後にユーザーストアを初期化

--- djob_frontend/app.vue
+++ djob_frontend/app.vue
@@ -1,3 +1,14 @@
+<script setup>
+import { onMounted } from "vue";
+import { useUserStore } from "@/stores/user";
+
+const userStore = useUserStore();
+onMounted(() => {
+  console.log("onMounted");
+  userStore.initStore();
+});
+</script>
+
 <template>
   <div>
     <NuxtLayout>
